6fb7dab3210e845a13e8164366567b6bd3d69d30
Minfeng <minfenglin0406@gmail.com>
2023-04-10 06:03:09 +0000

    fix some bug

--- cmd/main.go
+++ cmd/main.go
@@ -28,6 +28,7 @@ func main() {
 	Env_Time, _ := strconv.ParseInt(os.Getenv("Timer_Minutes"), 10, 64)
 	crontab_time := os.Getenv("Crontime")
 
+	go bot_service.Telegram_reply_run(&chatID, &yourToken)
 	if len(crontab_time) > 0 {
 		fmt.Printf("chatID: %d, yourToken: %s, Use crontab: %s \n", chatID, yourToken, crontab_time)
 		c := cron.New()
@@ -37,13 +38,11 @@ func main() {
 		})
 		momo_data := momo.Momo_parser_data()
 		bot_service.Telegram_bot_run(chatID, yourToken, momo_data)
-		go bot_service.Telegram_reply_run(&chatID, &yourToken)
 		c.Start()
 		select {}
 	} else {
 		fmt.Printf("chatID: %d, yourToken: %s, How often to run: %d (Minutes)\n", chatID, yourToken, Env_Time)
 		tChannel := time.NewTimer(time.Duration(Env_Time) * time.Minute)
-		go bot_service.Telegram_reply_run(&chatID, &yourToken)
 		for {
 			momo_data := momo.Momo_parser_data()
 			bot_service.Telegram_bot_run(chatID, yourToken, momo_data)
